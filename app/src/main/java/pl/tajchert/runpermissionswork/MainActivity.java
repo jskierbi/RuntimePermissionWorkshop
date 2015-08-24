dcf03731841e42665facbf9c248133feec870cc6
Michal Tajchert <mtajchert@gmail.com>
2015-08-24 09:29:10 +0200

    Solved second DIY - asking for permission and handling its result.

--- app/src/main/java/pl/tajchert/runpermissionswork/MainActivity.java
+++ app/src/main/java/pl/tajchert/runpermissionswork/MainActivity.java
@@ -1,8 +1,11 @@
 package pl.tajchert.runpermissionswork;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -14,6 +17,7 @@ import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
+    private static final int REQUEST_CODE_PHONE = 121;
 
     @Bind(R.id.main_layout)
     View mLayout;//We will use it later
@@ -27,12 +31,29 @@ public class MainActivity extends AppCompatActivity {
 
     @OnClick(R.id.buttonCall)
     public void clickButtCall() {
-        //TODO Ok so when targeting API=23 we will not have "out of the box" permission to do stuff.
-        //Lets try ask user for it!
-        //Try yourself, if not move to next commit
-        //Useful methods: ActivityCompat.checkSelfPermission(), ActivityCompat.requestPermissions(),
-        //and remember to handle result in onRequestPermissionsResult()
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) == PackageManager.PERMISSION_GRANTED) {
+            callTest();
+        } else {
+            //We don't, lets ask for it. Result will be thrown in onRequestPermissionsResult()
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CALL_PHONE}, REQUEST_CODE_PHONE);
+        }
+    }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        //Called when user presses "Allow" or "Deny" in permission request, array of permissions
+        // and result is there as those request can be merged together - and then one result is
+        // thrown for whole batch.
+        boolean hasAccess;
+        switch ( requestCode ) {
+            case(REQUEST_CODE_PHONE):
+                callTest();
+                break;
+            //Here goes other permissions, for example if you ask for Location  permission, here handle as well result
+        }
+    }
+
+    private void callTest() {
         Intent intent = new Intent(Intent.ACTION_CALL);
         intent.setData(Uri.parse("tel:" + "11122233"));
         try {
